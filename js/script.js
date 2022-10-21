689a84bab2271a36082c148aae50fb82bfd886e9
mlietti1 <mcf.lietti@outlook.com>
2022-10-21 16:59:37 +0200

    create bombs

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ const playBtn = document.querySelector('#play');
 const levelSelect = document.querySelector('#level'); //select element in html
 
 const gridLevels = [100, 81, 49];
+const BOMBS_NUMBER = 16;
+let bombs = [];
+let score = 0;
 
 
 playBtn.addEventListener('click', play);
@@ -15,6 +18,9 @@ function play(){
 
   //generare la griglia
   generatePlayground(cellNumbers);
+
+  // creo le bombe
+  bombs = generateBombs(cellNumbers);
 
 }
 
@@ -58,7 +64,28 @@ function handleClickCell(){
   this.classList.add('clicked'); //css per accendere cella cliccata
 }
 
+function generateBombs(cellNumbers){
+  // creo un array dove pushare le bombe che creo
+  const bombGenerated = [];
+
+  // ciclo finché ho 16 bombe 
+  while (bombGenerated.length < BOMBS_NUMBER){
+    const bomb = getRandomNumber(1, cellNumbers);
+
+    // se numero bomba estratto non è nell'array delle bombe lo pusho
+    if(!bombGenerated.includes(bomb)){
+      bombGenerated.push(bomb);
+    }
+  }
+}
+
 function reset(){
   main.innerHTML = '';
   document.querySelector('.endMessage').innerHTML = '';
+}
+
+// creo function per numero random per generare le bombe
+
+function getRandomNumber(min, max){
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
